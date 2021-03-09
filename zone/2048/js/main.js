134ba24a96a38327048d8a6fec12d52bff4c2eb6
X-varywow <1641323338@qq.com>
2021-03-09 21:01:05 +0800

    Site updated: 2021-03-09 21:01:04

---
+++ zone/2048/js/main.js
@@ -0,0 +1,181 @@
+var val = new Array();
+var add = new Array();
+var score = 0;
+var startX = 0;
+var startY = 0;
+var direction = 0;
+
+$(document).ready(function (e) {
+	init();
+});
+
+// 初始化
+function init() {
+	startX = 0;
+	startY = 0;
+	direction = 0;
+	score = 0;
+	for (var i = 0; i < 4; i++) {
+		val[i] = new Array();
+		add[i] = new Array();
+		for (var j = 0; j < 4; j++) {
+			val[i][j] = 0;
+			add[i][j] = 0;
+			$("#grid_cell_" + i + "_" + j).css("top", getTop(i));
+			$("#grid_cell_" + i + "_" + j).css("left", getLeft(j));
+		}
+	}
+	changeScore();
+	changeBestScore();
+	// 隐藏游戏结束面板
+	$("#body").css("opacity", "1");
+	var over = $("#over");
+	over.css("opacity", "0");
+	over.css("display", "none");
+	$(".number_cell, .grid_cell").css("opacity", "1");
+	generateNumberCell(); // 布局numberCell格子
+	// 随机选择一个位置并获取两个随机数
+	generateNumber();
+	generateNumber();
+	// 绑定监听事件
+	var content = document.getElementById('content');
+	content.addEventListener('touchstart', touchStart);
+	content.addEventListener('touchmove', touchMove, {passive: false});
+	content.addEventListener('touchend', touchEnd);
+	document.addEventListener('keydown', pressKeyBoard);
+}
+
+function touchStart(e) {
+	startX = e.touches[0].pageX;
+	startY = e.touches[0].pageY;
+}
+
+function touchMove(e) {
+	let endX = e.touches[0].pageX;
+	let endY = e.touches[0].pageY;
+	let dx = endX - startX;
+	let dy = endY - startY;
+	// 按四个象限求出开始结束两个点所成线段的角度
+	let angle = -Math.atan2(dy, dx) * 180 / Math.PI;
+	direction = judgeDirection(angle);
+	// 设置passive为false来提前告知浏览器使用preventDefault阻止默认行为
+	e.preventDefault();
+}
+
+function touchEnd(e) {
+	if (direction !== 0) {
+		move(direction);
+	}
+}
+
+function pressKeyBoard(e) {
+	move(e.keyCode);
+}
+
+// 生成数字的div
+function generateNumberCell() {
+	$(".number_cell").remove();
+	for (var i = 0; i < 4; i++) {
+		for (var j = 0; j < 4; j++) {
+			$(".body").append('<div class="number_cell" id="number_cell_' + i + '_' + j + '"></div>');
+			var numberCell = $("#number_cell_" + i + "_" + j);
+			if (val[i][j] == 0) {
+				numberCell.css("width", "0px");
+				numberCell.css("height", "0px");
+			} else {
+				numberCell.css("width", "21.25%;");
+				numberCell.css("height", "21.25%;");
+				numberCell.css("background-color", getNumberCellColor(val[i][j]));
+				numberCell.css("color", getNumberColor(val[i][j]));
+				numberCell.text(val[i][j]);
+			}
+			numberCell.css("top", getTop(i));
+			numberCell.css("left", getLeft(j));
+		}
+	}
+}
+
+// 生成随机数
+function generateNumber() {
+	if (noSpace()) return false; //判断是否还有空格子
+	var randomX = parseInt(Math.floor(Math.random() * 4));
+	var randomY = parseInt(Math.floor(Math.random() * 4));
+	while (true) {
+		if (val[randomX][randomY] == 0) break;
+		else {
+			randomX = _.random(0, 3);
+			randomY = _.random(0, 3);
+		}
+	}
+	var randomNumber = (Math.random() < 0.5) ? 2 : 4;
+	val[randomX][randomY] = randomNumber;
+	showNumberWithAnimation(randomX, randomY, randomNumber); //布局
+	return true;
+}
+
+// 格子移动
+function move(key) {
+	switch (key) {
+		case 37:
+		case 4:
+		  if(!canMoveLeft())  return false;
+			moveLeft();
+			break;
+		case 39:
+		case 2:
+			if(!canMoveRight())  return false;
+			moveRight();
+			break;
+		case 38:
+		case 1:
+			if(!canMoveUp())  return false;
+			moveUp();
+			break;
+		case 40:
+		case 3:
+			if(!canMoveDown())  return false;
+			moveDown();
+			break;
+		// 防止按下其他键后依旧生成新数字 
+		default: 
+			return
+	}
+	changeScore();
+	setTimeout(function () {
+		// 等移动动画过后再生成数字，否则若合并和生成数字是在同一行发生的会出现格子“后退”错觉
+		generateNumberCell();
+		generateNumber();
+		isGameOver();
+	}, 200);
+}
+
+//实时改变分数
+function changeScore() {
+	$("#score").html(score);
+	changeBestScore();
+}
+
+// 实时改变最高分数
+function changeBestScore() {
+	let tem = localStorage.getItem('2048_bestScore') || 0;
+	if (score > tem) {
+		tem = score;
+		localStorage.setItem('2048_bestScore', score);
+	}
+	$('#bestScore').html(tem);
+}
+
+// 判断是往哪个方向的移动
+function judgeDirection(angle) {
+	if (angle >= 45 && angle <= 135) {
+		return 1;
+	} else if (angle < 45 && angle >= -45) {
+		return 2;
+	} else if (angle < -45 && angle >= -135) {
+		return 3;
+	} else if ((angle < -135 && angle >= -180) || (angle > 135 && angle <= 180)) {
+		return 4;
+	} else {
+		return 0;
+	}
+}
---
+++ zone/2048/js/supportFunction.js
@@ -0,0 +1,334 @@
+//逐渐显示随机数字
+function showNumberWithAnimation(i, j, number){
+	var numberCell= $("#number_cell_" + i + "_" + j);	
+	numberCell.css("background-color", getNumberCellColor(number));
+	numberCell.css("color", getNumberColor(number));
+	numberCell.text(number);
+	numberCell.animate({
+		width: "21.25%",
+		height: "21.25%",
+		top: getTop(i),
+		left: getLeft(j),
+	}, 100);
+}
+
+//数字方格移动动画
+function showMoveWithAnimation(fromRow, toRow, fromCol, toCol){
+	var numberCell= $("#number_cell_" + fromRow + "_" + fromCol);
+	numberCell.animate({
+		left: getLeft(toCol),
+		top: getTop(toRow),
+	}, 100);
+}
+
+//数字方格的背景颜色
+function getNumberCellColor(number){
+	switch(number){
+		case 2: 
+            return "#eee4da";
+            break;
+        case 4:
+            return "#eee4da";
+            break;
+        case 8:
+            return "#f26179";
+            break;
+        case 16:
+            return "#f59563";
+            break;
+        case 32:
+            return "#f67c5f";
+            break;
+        case 64:
+            return "#f65e36";
+            break;
+        case 128:
+            return "#edcf72";
+            break;
+        case 256:
+            return "#edcc61";
+            break;
+        case 512:
+            return "#9c0";
+            break;
+        case 1024:
+            return "#3365a5";
+            break;
+        case 2048:
+            return "#09c";
+            break;
+        case 4096:
+            return "#a6bc";
+            break;
+        case 8192:
+            return "#93c";
+            break;
+    }
+    return "black";
+}
+
+//数字颜色
+function getNumberColor(number){
+	if(number <=4)  return "#776e65";
+	else  return "white";
+}
+
+//numberCell离顶部的距离
+function getTop(i){
+	return  `${3 + 24.25 * i}%`;
+}
+
+//numberCell离左边的距离
+function getLeft(j){
+	return  `${3 + 24.25 * j}%`;
+}
+
+//判断是否还有空格子
+function noSpace(){
+	for(var i=0 ;i<4; i++){
+		for(var j=0; j<4; j++){
+			if(val[i][j]== 0)  return false;
+		}
+	}
+	return true;
+}
+
+//判断是否还有可以移动的盒子
+function noMove(){
+	if(canMoveLeft() || canMoveRight() || canMoveUp() || canMoveDown()) return false;
+	else    return true;
+}
+
+//判断是否游戏结束
+function isGameOver(){
+	if(noSpace() && noMove()) gameOver();
+}
+
+//游戏结束
+function gameOver(){
+	$("#body").css("opacity", "0.5");
+	var over= $("#over");     
+	over.css("display", "block");	
+	over.animate({
+		"opacity": "1"
+	}, 500);
+	// 需要移除将之前的监听事件，否则重新开始游戏后会造成监听事件的重叠多次触发
+	var content = document.getElementById('content');
+	content.removeEventListener('touchstart', touchStart);
+	content.removeEventListener('touchmove', touchMove, {passive: false});
+	content.removeEventListener('touchend', touchEnd);
+	document.removeEventListener('keydown',pressKeyBoard);
+}
+
+//向左移动
+function moveLeft(){
+	initAdd();                                     //将判断是否要合并的add数组重置为0
+	for(var i=0; i<4; i++){
+		for(var j=1; j<4; j++){
+			if(val[i][j] != 0 ){
+				for(var t=0; t<j; t++){
+					if(val[i][t] == 0 && noBlockHorizontal(i, t, j)){     //到达位置为空而且中间没有障碍物
+						showMoveWithAnimation(i, i, j, t);                //第i行到第i行，第j列到第t列
+					    val[i][t]= val[i][j];
+						val[i][j]= 0;
+						break;
+					}
+					else if(val[i][t] == val[i][j] && noBlockHorizontal(i, t, j)){  
+						if(add[i][t] == 0){                               //可以和到达位置的方格合并
+							showMoveWithAnimation(i, i, j, t);
+							val[i][t]+= val[i][j];
+							score+= val[i][t];
+						    val[i][j]= 0;
+							add[i][t]= 1;
+						}
+						else{                   //到达位置的方格已经被合并过，退而占据右边的方格
+							showMoveWithAnimation(i, i, j, t+1);
+							val[i][t+1]= val[i][j];
+							val[i][j]= 0;
+						}
+						break;
+					}
+				}
+			}
+		}
+	}
+	return true;
+}
+
+//向右移动
+function moveRight(){
+	initAdd();
+	for(var i=0; i<4; i++){
+		for(var j=2; j>=0; j--){
+			if(val[i][j] != 0 ){
+				for(var t=3; t>j; t--){
+					if(val[i][t] == 0 && noBlockHorizontal(i, j, t)){     
+						showMoveWithAnimation(i, i, j, t);                
+					    val[i][t]= val[i][j];
+						val[i][j]= 0;
+						break;
+					}
+					else if(val[i][t] == val[i][j] && noBlockHorizontal(i, j, t)){  
+						if(add[i][t] == 0){                              
+							showMoveWithAnimation(i, i, j, t);
+							val[i][t]+= val[i][j];
+							score+= val[i][t];
+						    val[i][j]= 0;
+							add[i][t]= 1;
+						}
+						else{                   
+							showMoveWithAnimation(i, i, j, t-1);
+							val[i][t-1]= val[i][j];
+							val[i][j]= 0;
+						}
+						break;
+					}
+				}
+			}
+		}
+	}
+	return true;
+}
+
+//向上移动
+function moveUp(){
+	initAdd();
+	for(var j=0; j<4; j++){
+		for(var i=1; i<4; i++){
+			if(val[i][j] != 0 ){
+				for(var t=0; t<i; t++){
+					if(val[t][j] == 0 && noBlockVertical(j, t, i)){     
+						showMoveWithAnimation(i, t, j, j);               
+					    val[t][j]= val[i][j];
+						val[i][j]= 0;
+						break;
+					}
+					else if(val[t][j] == val[i][j] && noBlockVertical(j, t, i)){  
+						if(add[t][j] == 0){                              
+							showMoveWithAnimation(i, t, j, j);
+							val[t][j]+= val[i][j];
+							score+= val[t][j];
+						    val[i][j]= 0;
+							add[t][j]= 1;
+						}
+						else{                   
+							showMoveWithAnimation(i, i, j, t+1);
+							val[t+1][j]= val[i][j];
+							val[i][j]= 0;
+						}
+						break;
+					}
+				}
+			}
+		}
+	}
+	return true;
+}
+
+//向下移动
+function moveDown(){
+	initAdd();
+	for(var j=0; j<4; j++){
+		for(var i=2; i>=0; i--){
+			if(val[i][j] != 0 ){
+				for(var t=3; t>i; t--){
+					if(val[t][j] == 0 && noBlockVertical(j, i, t)){     
+						showMoveWithAnimation(i, t, j, j);              
+					    val[t][j]= val[i][j];
+						val[i][j]= 0;
+						break;
+					}
+					else if(val[t][j] == val[i][j] && noBlockVertical(j, i, t)){  
+						if(add[t][j] == 0){                               
+							showMoveWithAnimation(i, t, j, j);
+							val[t][j]+= val[i][j];
+							score+= val[t][j];
+						    val[i][j]= 0;
+							add[t][j]= 1;
+						}
+						else{                   
+							showMoveWithAnimation(i, i, j, t-1);
+							val[t-1][j]= val[i][j];
+							val[i][j]= 0;
+						}
+						break;
+					}
+				}
+			}
+		}
+	}
+	return true;
+}
+
+//恢复判断是否要合并的add数组为初始状态
+function initAdd(){
+	for(var i=0; i<4; i++){
+		for(var j=0; j<4; j++){
+			add[i][j]= 0;
+		}
+	}
+}
+
+//判断格子是否可以向左移动
+function canMoveLeft(){
+	for(var i=0; i<4; i++){
+		for(var j=1; j<4; j++){
+			if(val[i][j] != 0){
+				if(val[i][j-1] == 0 || val[i][j-1] == val[i][j])  return true;
+			}
+		}
+	}
+	return false;
+}
+
+//判断格子是否可以向右移动
+function canMoveRight(){
+	for(var i=0; i<4; i++){
+		for(var j=2; j>=0; j--){
+			if(val[i][j] != 0){
+				if(val[i][j+1] == 0 || val[i][j+1] == val[i][j])  return true;
+			}
+		}
+	}
+	return false;
+}
+
+//判断格子是否可以向上移动
+function canMoveUp(){
+	for(var i=1; i<4; i++){
+		for(var j=0; j<4; j++){
+			if(val[i][j] != 0){
+				if(val[i-1][j] == 0 || val[i-1][j] == val[i][j])  return true;
+			}
+		}
+	}
+	return false;
+}
+
+//判断格子是否可以向下移动
+function canMoveDown(){
+	for(var i=2; i>=0; i--){
+		for(var j=0; j<4; j++){
+			if(val[i][j] != 0){
+				if(val[i+1][j] == 0 || val[i+1][j] == val[i][j])  return true;
+			}
+		}
+	}
+	return false;
+}
+
+//判断水平方向上两个格子之间有没有障碍物
+function noBlockHorizontal(row, fromCol, toCol){
+	for(var i=fromCol+1; i<toCol; i++){
+		if(val[row][i] !=0)  return false;
+	}
+	return true;
+}
+
+//判断竖直方向上两个格子之间有没有障碍物
+function noBlockVertical(col, fromRow, toRow){
+	for(var i=fromRow+1; i<toRow; i++){
+		if(val[i][col] !=0)  return false;
+	}
+	return true;
+}
